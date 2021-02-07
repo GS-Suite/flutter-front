390b198d7af6314181114cb9cafbd31368bc87e7
FIRST_NAME LAST_NAMEjaisal1311 <jaisalashah123@gmail.com>
2021-02-07 21:24:03 +0530

    feat: opens dashboard if already logged in else login

--- gssuite/lib/Components/Login_Screen/login.dart
+++ gssuite/lib/Components/Login_Screen/login.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gssuite/Components/Dashboard/dashboard.dart';
 import 'package:gssuite/Components/Login_Screen/TitleComponent.dart';
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'TextField_Component.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class SignIn extends StatefulWidget {
   SignIn({Key key, this.title}) : super(key: key);
@@ -13,8 +16,37 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignIn extends State<SignIn> {
+  static bool _isLoggedIn;
+
+  isLoggedIn() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    print(prefs.getString('username'));
+    print('init');
+    setState(() {
+      _isLoggedIn = prefs.getString('username') == null ? false : true;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isLoggedIn();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(_isLoggedIn);
+    return _isLoggedIn != true
+        ? _isLoggedIn == false
+            ? main()
+            : Center(
+                child: SpinKitWanderingCubes(
+                color: Colors.blueAccent,
+              ))
+        : Dashboard();
+  }
+
+  Widget main() {
     return Scaffold(
       body: ColorfulSafeArea(
         color: Colors.white,
