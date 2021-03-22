572638e8a6b6244bfda8b6e7feec12e82b344112
FIRST_NAME LAST_NAMEjaisal1311 <jaisalashah123@gmail.com>
2021-03-22 10:32:00 +0530

    fix: drawer ready after create class

--- gssuite/lib/Components/Drawer Component/drawer.dart
+++ gssuite/lib/Components/Drawer Component/drawer.dart
@@ -47,10 +47,6 @@ class AppDrawer extends StatelessWidget {
         children: <Widget>[
           _createHeader(),
           _createDrawerItem(
-              icon: Icons.create_new_folder_outlined,
-              text: 'Create Class',
-              tap: () => Navigator.pushNamed(context, '/create_class')),
-          _createDrawerItem(
             icon: Icons.note_outlined,
             text: 'Saved Notes',
           ),
