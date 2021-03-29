da99dfc7bcdee00735bb61deb92dc550323ed688
FIRST_NAME LAST_NAMEjaisal1311 <jaisalashah123@gmail.com>
2021-03-30 00:40:37 +0530

    util ui fix

--- gssuite/lib/Components/Dashboard/dashboard.dart
+++ gssuite/lib/Components/Dashboard/dashboard.dart
@@ -100,173 +100,176 @@ class _DashboardState extends State<Dashboard> {
         ),
       ));
     } else {
-      return Scaffold(
-          key: _scaffoldKey,
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            centerTitle: false,
-            elevation: 0,
-            backgroundColor: Colors.white,
-            title: Padding(
-              padding: const EdgeInsets.only(left: 0, top: 15),
-              child: GestureDetector(
-                onTap: () => _scaffoldKey.currentState.openDrawer(),
-                child: RichText(
-                  textAlign: TextAlign.start,
-                  text: TextSpan(
-                    text: '# ',
-                    style: TextStyle(
-                        color: Colors.teal[400],
-                        fontWeight: FontWeight.bold,
-                        fontSize: 33),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: _user.contains(' ')
-                              ? 'Hi, ' +
-                                  _user.substring(0, _user.indexOf(' ')) +
-                                  '!'
-                              : _user, // To be changed
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black)),
-                    ],
+      return WillPopScope(
+        onWillPop: _onBackPressed,
+        child: Scaffold(
+            key: _scaffoldKey,
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              centerTitle: false,
+              elevation: 0,
+              backgroundColor: Colors.white,
+              title: Padding(
+                padding: const EdgeInsets.only(left: 0, top: 15),
+                child: GestureDetector(
+                  onTap: () => _scaffoldKey.currentState.openDrawer(),
+                  child: RichText(
+                    textAlign: TextAlign.start,
+                    text: TextSpan(
+                      text: '# ',
+                      style: TextStyle(
+                          color: Colors.teal[400],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 33),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: _user.contains(' ')
+                                ? 'Hi, ' +
+                                    _user.substring(0, _user.indexOf(' ')) +
+                                    '!'
+                                : _user, // To be changed
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black)),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          drawer: AppDrawer(),
-          body: Container(
-            alignment: Alignment.topLeft,
-            width: double.infinity,
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: _userClassrooms != null
-                  ? Column(
-                      children: [
-                        SubscribedCourses(
-                          title: 'Your Classes',
-                          classrooms: _userClassrooms,
-                          enrolled: false,
-                        ),
-                        SizedBox(height: 20.0),
-                        SubscribedCourses(
-                          title: 'Enrolled Classes',
-                          classrooms: _userEnrolledClasrooms,
-                          enrolled: true,
-                        ),
-                      ],
-                    )
-                  : Center(
-                      child: SpinKitThreeBounce(
-                        color: Colors.teal[400],
-                      ),
-                    ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(
-              Icons.add,
+            drawer: AppDrawer(),
+            body: Container(
+              alignment: Alignment.topLeft,
+              width: double.infinity,
               color: Colors.white,
-            ),
-            backgroundColor: Colors.teal[400],
-            onPressed: () => {
-              showDialog(
-                  context: context,
-                  builder: (_) => AlertDialog(
-                          content: Container(
-                              child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: _userClassrooms != null
+                    ? Column(
                         children: [
-                          GestureDetector(
-                            onTap: () async => {
-                              Navigator.of(context, rootNavigator: true)
-                                  .pop('dialog'),
-                              await Navigator.pushNamed(
-                                  context, '/create_class'),
-                              setState(() => {pref()})
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(15.0),
-                              height: 100,
-                              width: 130,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Center(
-                                    child: Icon(
-                                      Icons.create_new_folder_outlined,
-                                      size: 35.0,
-                                      color: Colors.teal[400],
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 5.0,
-                                  ),
-                                  Center(
-                                    child: Text('Create class',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.bold)),
-                                  )
-                                ],
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.grey[100],
-                                shape: BoxShape.rectangle,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8.0)),
-                              ),
-                            ),
+                          SubscribedCourses(
+                            title: 'Your Classes',
+                            classrooms: _userClassrooms,
+                            enrolled: false,
                           ),
-                          SizedBox(
-                            width: 10,
+                          SizedBox(height: 20.0),
+                          SubscribedCourses(
+                            title: 'Enrolled Classes',
+                            classrooms: _userEnrolledClasrooms,
+                            enrolled: true,
                           ),
-                          GestureDetector(
-                            onTap: () => {
-                              Navigator.of(context, rootNavigator: true)
-                                  .pop('dialog'),
-                              Navigator.pushNamed(context, '/join_class')
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(15.0),
-                              width: 130,
-                              height: 100,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Center(
-                                    child: Icon(
-                                      Icons.arrow_circle_up_sharp,
-                                      size: 35.0,
-                                      color: Colors.teal[400],
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 5.0,
-                                  ),
-                                  Center(
-                                    child: Text('Join class',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.bold)),
-                                  )
-                                ],
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.grey[100],
-                                shape: BoxShape.rectangle,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8.0)),
-                              ),
-                            ),
-                          )
                         ],
-                      ))))
-            },
-            splashColor: Colors.teal[300],
-          ));
+                      )
+                    : Center(
+                        child: SpinKitThreeBounce(
+                          color: Colors.teal[400],
+                        ),
+                      ),
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              backgroundColor: Colors.teal[400],
+              onPressed: () => {
+                showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(
+                            content: Container(
+                                child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            GestureDetector(
+                              onTap: () async => {
+                                Navigator.of(context, rootNavigator: true)
+                                    .pop('dialog'),
+                                await Navigator.pushNamed(
+                                    context, '/create_class'),
+                                setState(() => {pref()})
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(15.0),
+                                height: 100,
+                                width: 130,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Center(
+                                      child: Icon(
+                                        Icons.create_new_folder_outlined,
+                                        size: 35.0,
+                                        color: Colors.teal[400],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Center(
+                                      child: Text('Create class',
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold)),
+                                    )
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[100],
+                                  shape: BoxShape.rectangle,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8.0)),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            GestureDetector(
+                              onTap: () => {
+                                Navigator.of(context, rootNavigator: true)
+                                    .pop('dialog'),
+                                Navigator.pushNamed(context, '/join_class')
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(15.0),
+                                width: 130,
+                                height: 100,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Center(
+                                      child: Icon(
+                                        Icons.arrow_circle_up_sharp,
+                                        size: 35.0,
+                                        color: Colors.teal[400],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Center(
+                                      child: Text('Join class',
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold)),
+                                    )
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[100],
+                                  shape: BoxShape.rectangle,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8.0)),
+                                ),
+                              ),
+                            )
+                          ],
+                        ))))
+              },
+              splashColor: Colors.teal[300],
+            )),
+      );
     }
   }
 
