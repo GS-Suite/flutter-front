7bb8e2c91ef1943191f9eaecdf5148575c1847fe
FIRST_NAME LAST_NAMEjaisal1311 <jaisalashah123@gmail.com>
2021-03-29 20:58:37 +0530

    attendance list on teacher's side Ui done

--- gssuite/lib/Components/Classroom/UserClassrooms/ViewAttendance.dart
+++ gssuite/lib/Components/Classroom/UserClassrooms/ViewAttendance.dart
@@ -3,6 +3,7 @@ import '../../Drawer Component/drawer.dart';
 import '../../../apis/api.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
 
 class ViewAttendance extends StatefulWidget {
@@ -17,6 +18,8 @@ class ViewAttendance extends StatefulWidget {
 class _ViewAttendanceState extends State<ViewAttendance> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   var _attendanceList = null;
+  var _val_list = [];
+  var _attendanceToken = '';
 
   @override
   void initState() {
@@ -28,40 +31,108 @@ class _ViewAttendanceState extends State<ViewAttendance> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          centerTitle: false,
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Padding(
-            padding: const EdgeInsets.only(left: 0, top: 15),
-            child: GestureDetector(
-              onTap: () => _scaffoldKey.currentState.openDrawer(),
-              child: RichText(
-                textAlign: TextAlign.start,
-                text: TextSpan(
-                  text: '# ',
-                  style: TextStyle(
-                      color: Colors.teal[400],
-                      fontWeight: FontWeight.bold,
-                      fontSize: 33),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: 'Attendance Logs', // To be changed
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.black)),
-                  ],
-                ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: false,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 0, top: 15),
+          child: GestureDetector(
+            onTap: () => _scaffoldKey.currentState.openDrawer(),
+            child: RichText(
+              textAlign: TextAlign.start,
+              text: TextSpan(
+                text: '# ',
+                style: TextStyle(
+                    color: Colors.teal[400],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 33),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Attendance Logs', // To be changed
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black)),
+                ],
               ),
             ),
           ),
         ),
-        drawer: AppDrawer(),
-        body: Container(
-          child: Center(
-            child: Text('sjugf'),
-          ),
-        ));
+      ),
+      drawer: AppDrawer(),
+      body: Container(
+        child: _attendanceList != null
+            ? Container(
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: _attendanceList.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 3),
+                        child: Container(
+                            margin: EdgeInsets.only(right: 10.0),
+                            width: 250,
+                            child: ListTile(
+                              visualDensity:
+                                  VisualDensity(horizontal: 0, vertical: -4),
+                              contentPadding: EdgeInsets.all(0),
+                              title: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 8.0, left: 0, bottom: 8.0),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            _attendanceList[index]
+                                                    .toString()
+                                                    .substring(11, 19) +
+                                                ' Hrs',
+                                            style: TextStyle(fontSize: 18),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            _attendanceList[index]
+                                                .toString()
+                                                .substring(0, 10),
+                                            style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.grey[600]),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              trailing: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: GestureDetector(
+                                    onTap: () => {},
+                                    child: Icon(Icons.chevron_right_sharp)),
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom:
+                                        BorderSide(color: Colors.black26)))),
+                      );
+                    }),
+              )
+            : Center(
+                child: SpinKitThreeBounce(
+                color: Colors.teal[400],
+              )),
+      ),
+    );
   }
 
   void getAttendanceList() async {
@@ -78,11 +149,18 @@ class _ViewAttendanceState extends State<ViewAttendance> {
     var res = json.decode(response.body.toString());
     if (res['success'] == true) {
       print(res);
+      var keys = res['data'].keys.toList();
       setState(() {
-        _attendanceList = res['data'];
+        _attendanceList = keys;
+        keys.forEach((date) => {
+              _val_list.add(res['data'][date.toString()]),
+            });
+        _attendanceToken = res['data']['token'];
       });
       print('attendance list');
       print(_attendanceList);
+      print('val_list');
+      print(_val_list);
       prefs.setString('token', res['token'].toString());
     } else {
       print('Response didn\'t fetch');
