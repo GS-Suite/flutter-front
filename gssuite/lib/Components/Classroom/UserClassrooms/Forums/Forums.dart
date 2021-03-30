78df90e44c77830e669d43f56b44c37d0025bbdf
FIRST_NAME LAST_NAMEjaisal1311 <jaisalashah123@gmail.com>
2021-03-30 18:17:28 +0530

    bottom keyboard textfield done; get_chat init

--- gssuite/lib/Components/Classroom/UserClassrooms/Forums/Forums.dart
+++ gssuite/lib/Components/Classroom/UserClassrooms/Forums/Forums.dart
@@ -3,12 +3,26 @@ import 'package:flutter/material.dart';
 class Forums extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text(
-        'Forums',
-        style: TextStyle(fontSize: 22, color: Colors.teal[400]),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Text('Forums'),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Chat message',
+              ),
+            ),
+          ),
+        ],
       ),
-    ));
+    );
   }
 }
