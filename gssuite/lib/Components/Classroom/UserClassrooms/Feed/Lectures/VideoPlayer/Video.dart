5cb5a26cfa7aea64a552c1504f8ce3599bd905cc
FIRST_NAME LAST_NAMEjaisal1311 <jaisalashah123@gmail.com>
2021-04-08 13:36:44 +0530

    feat: dynamically forcing youtbe to load on full screen

--- gssuite/lib/Components/Classroom/UserClassrooms/Feed/Lectures/VideoPlayer/Video.dart
+++ gssuite/lib/Components/Classroom/UserClassrooms/Feed/Lectures/VideoPlayer/Video.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class Video extends StatefulWidget {
@@ -11,19 +10,34 @@ class Video extends StatefulWidget {
 }
 
 class _VideoState extends State<Video> {
+  var _id;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _id = this.widget.url.toString().substring(8).startsWith('www')
+        ? this.widget.url.toString().substring(12).startsWith('you')
+            ? this.widget.url.toString().substring(
+                  this.widget.url.length - 11,
+                )
+            : false
+        : this.widget.url.toString().substring(8).startsWith('you')
+            ? this.widget.url.toString().substring(
+                  this.widget.url.length - 11,
+                )
+            : false;
   }
 
   InAppWebViewController webView;
 
   @override
   Widget build(BuildContext context) {
+    print(this.widget.url.toString().substring(8));
     return Container(
       child: InAppWebView(
-        initialUrl: this.widget.url,
+        initialUrl: this._id == false
+            ? this.widget.url
+            : 'https://www.youtube.com/embed/' + _id.toString(),
         onWebViewCreated: (InAppWebViewController controller) {
           webView = controller;
         },
